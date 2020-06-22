90229e6759d913f1dafed41e28b272119f04cdbe
Christoph Pickl <christoph.pickl@gmail.com>
2020-06-22 13:12:36 +0200

    setup gradle

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+plugins {
+    kotlin("jvm") version Versions.kotlin
+    id("com.github.ben-manes.versions") version Versions.Plugins.versions
+    application
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    implementation("io.github.microutils:kotlin-logging:${Versions.klogging}")
+    implementation("ch.qos.logback:logback-classic:${Versions.logback}")
+
+    testImplementation("org.testng:testng:${Versions.testng}")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:${Versions.assertk}")
+    testImplementation("io.mockk:mockk:${Versions.mockk}")
+}
+
+application {
+    mainClassName = "iptiq.LoadBalancerApp"
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjsr305=strict", "-Xuse-experimental=io.ktor.util.KtorExperimentalAPI")
+        }
+    }
+
+
+    withType<Test> {
+        useTestNG {}
+    }
+
+    withType<DependencyUpdatesTask> {
+        val rejectPatterns = listOf("alpha", "beta", "eap", "rc").map { qualifier ->
+            Regex("(?i).*[.-]$qualifier[.\\d-]*")
+        }
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    if (rejectPatterns.any { it.matches(candidate.version) }) {
+                        reject("Release candidate")
+                    }
+                }
+            }
+        }
+        checkForGradleUpdate = true
+    }
+}
