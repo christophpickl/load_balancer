810dd8504617e4b0d90e8c164d69625f028656e8
Christoph Pickl <christoph.pickl@gmail.com>
2020-06-22 13:17:45 +0200

    finish gradle setup

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,12 @@
+object Versions {
+    const val assertk = "0.20"
+    const val klogging = "1.7.8"
+    const val kotlin = "1.3.61"
+    const val logback = "1.2.3"
+    const val mockk = "1.9.3"
+    const val testng = "7.1.1"
+
+    object Plugins {
+        const val versions = "0.27.0"
+    }
+}
---
+++ src/main/kotlin/iptiq/LoadBalancerApp.kt
@@ -0,0 +1,8 @@
+package iptiq
+
+object LoadBalancerApp {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Load Balancer.")
+    }
+}
