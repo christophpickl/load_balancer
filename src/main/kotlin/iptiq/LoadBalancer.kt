3fdc9d26b912e529131c6614e8cc09a180426822
Christoph Pickl <christoph.pickl@gmail.com>
2020-06-22 13:45:03 +0200

    step 2: implement maximum providers

--- src/main/kotlin/iptiq/LoadBalancer.kt
+++ src/main/kotlin/iptiq/LoadBalancer.kt
@@ -1,5 +1,9 @@
 package iptiq
 
+import java.lang.Exception
+
+private const val MAXIMUM_PROVIDERS = 10
+
 class LoadBalancer {
 
     private val registeredProviders = mutableListOf<Provider>()
@@ -7,9 +11,12 @@ class LoadBalancer {
     val providers: List<Provider> get() = registeredProviders
 
     fun register(providers: List<Provider>) {
+        if (registeredProviders.size >= MAXIMUM_PROVIDERS) {
+            throw OutOfProviderException()
+        }
         registeredProviders += providers
     }
 
 }
 
-class OutOfProviderException : Exception("Maximum number of possible providers reached!")
+class OutOfProviderException : Exception("Maximum number of $MAXIMUM_PROVIDERS possible providers reached!")
