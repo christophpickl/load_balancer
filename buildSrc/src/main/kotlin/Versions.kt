a34b04f913dd30efc63cb4c6adeb7bb93310c82c
Christoph Pickl <christoph.pickl@gmail.com>
2020-06-22 13:30:48 +0200

    upgrade dependency version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
-    const val assertk = "0.20"
-    const val klogging = "1.7.8"
-    const val kotlin = "1.3.61"
+    const val assertk = "0.22"
+    const val klogging = "1.7.10"
+    const val kotlin = "1.3.72"
     const val logback = "1.2.3"
-    const val mockk = "1.9.3"
-    const val testng = "7.1.1"
+    const val mockk = "1.10.0"
+    const val testng = "7.2.0"
 
     object Plugins {
-        const val versions = "0.27.0"
+        const val versions = "0.28.0"
     }
 }
