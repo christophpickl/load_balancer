ee0683568cc99626fbd60f913c3bacc367e15fba
Christoph Pickl <christoph.pickl@gmail.com>
2020-06-22 14:36:46 +0200

    step 5: provider can have boolean exclusion state

--- src/main/kotlin/iptiq/Provider.kt
+++ src/main/kotlin/iptiq/Provider.kt
@@ -6,10 +6,23 @@ class Provider(
     private val id: ProviderId = ProviderId.random()
 ) {
 
-    companion object // for test instances
+    companion object {} // for test instances
+
+    private var _excluded = false
+    val excluded get() = _excluded
 
     /** Unique identifier of this instance */
     fun get() = id
+
+    fun include() = apply {
+        check(_excluded)
+        _excluded = false
+    }
+
+    fun exclude() = apply {
+        check(!_excluded)
+        _excluded = true
+    }
 
     override fun toString() = "Provider[id=$id]"
 
--- src/test/kotlin/iptiq/ProviderTest.kt
+++ src/test/kotlin/iptiq/ProviderTest.kt
@@ -1,7 +1,11 @@
 package iptiq
 
 import assertk.assertThat
+import assertk.assertions.isFailure
+import assertk.assertions.isFalse
+import assertk.assertions.isInstanceOf
 import assertk.assertions.isNotEqualTo
+import assertk.assertions.isTrue
 import org.testng.annotations.Test
 
 @Test
@@ -12,6 +16,46 @@ class ProviderTest {
         val id2 = Provider().get()
 
         assertThat(id1).isNotEqualTo(id2)
+    }
+
+    fun `Given default provider When check excluded Then should be false`() {
+        val provider = Provider()
+
+        val isExcluded = provider.excluded
+
+        assertThat(isExcluded).isFalse()
+    }
+
+    fun `Given included provider When exclude Then change state`() {
+        val provider = Provider()
+
+        provider.exclude()
+
+        assertThat(provider.excluded).isTrue()
+    }
+
+    fun `Given excluded provider When include Then change state`() {
+        val provider = Provider().exclude()
+
+        provider.include()
+
+        assertThat(provider.excluded).isFalse()
+    }
+
+    fun `Given default provider When include Then fail`() {
+        val provider = Provider()
+
+        assertThat {
+            provider.include()
+        }.isFailure().isInstanceOf(IllegalStateException::class)
+    }
+
+    fun `Given excluded provider When exclude Then fail`() {
+        val provider = Provider().exclude()
+
+        assertThat {
+            provider.exclude()
+        }.isFailure().isInstanceOf(IllegalStateException::class)
     }
 
 }
